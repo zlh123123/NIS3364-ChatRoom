6a50b8a44327b6f8d78fff9bf4dd4c5002458e07
zlh123123 <zhanglinghao@sjtu.edu.cn>
2024-12-07 16:34:47 +0800

    initial login register

---
+++ client/function/login_register.py
@@ -0,0 +1,110 @@
+import threading
+import hashlib
+import socket
+import time
+
+import function.utils as utils
+
+from PyQt5.QtWidgets import QMessageBox
+import re
+from Ui_py.Ui_LoginWindow import showdialog
+
+s = None
+user = ""
+
+
+def shutdown():
+    global s
+    if s:
+        utils.send(s, {"action": "shutdown"})
+        s.shutdown(2)
+        s.close()
+        s = None
+
+
+def ishostright(host, port):
+    # 检查host是否为合法的ip地址, port是否为合法的端口号
+    if (
+        re.match(
+            r"^(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)\.(25[0-5]|2[0-4]\d|[01]?\d\d?)$",
+            host,
+        )
+        and 0 < int(port) < 65536
+    ):
+        return True
+
+    return False
+
+
+def login(host, port, username, password):
+    if not ishostright(host, port):
+        QMessageBox.critical(None, "登录失败", "请输入正确的ip地址或端口号！")
+        
+        return
+    global s, user
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)  # 设置超时时间
+    if username != "" and password != "":
+        s.connect((host, int(port)))
+        utils.send(
+            s,
+            {
+                "action": "login",
+                "username": username,
+                "password": hashlib.md5(password.encode()).hexdigest(),  # 用md5加密密码
+            },
+        )
+        response = utils.recv(s)
+        if response["response"] == "ok":
+            user = username
+            utils.send(s, {"action": "get_all_users"})
+            utils.send(s, {"action": "get_all_history", "object": ""})
+
+            # 开启异步进程，根据服务器返回的数据进行处理
+            t = threading.Thread(target=handle_server_response, args=())
+            t.daemon = True  # 设置为守护线程，主线程结束时，守护线程也会结束
+            t.start()
+            # 显示登录成功的对话框
+            QMessageBox.information(None, "登录成功", "您已成功登录！")
+        elif response["response"] == "fail":
+            QMessageBox.critical(None, "登录失败", response["reason"])
+
+            shutdown()
+
+    else:
+        QMessageBox.critical(None, "登录失败", "用户名或密码不能为空！")
+        shutdown()
+
+
+def register(host, port, username, password):
+    if not ishostright(host, port):
+        QMessageBox.critical(None, "登录失败", "请输入正确的ip地址或端口号！")
+        return
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+    if username != "" and password != "":
+        s.connect((host, int(port)))
+        utils.send(
+            s,
+            {
+                "action": "register",
+                "username": username,
+                "password": hashlib.md5(password.encode()).hexdigest(),
+            },
+        )
+        response = utils.recv(s)
+        if response["response"] == "ok":
+            QMessageBox.information(None, "注册成功", "您已成功注册！")
+
+        elif response["response"] == "fail":
+            QMessageBox.critical(None, "注册失败", response["reason"])
+
+    else:
+        QMessageBox.critical(None, "注册失败", "用户名或密码不能为空！")
+
+    shutdown()
+
+
+def handle_server_response():
+    pass
---
+++ server/server.py
@@ -0,0 +1,162 @@
+import socketserver
+import pickle
+import time
+import sqlite3
+
+import utils
+
+# 数据库初始化
+conn = sqlite3.connect("chat.db")
+c = conn.cursor()
+c.execute(
+    """CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)"""
+)
+c.execute(
+    """CREATE TABLE IF NOT EXISTS history (sender TEXT, receiver TEXT, timestamp TEXT, message TEXT)"""
+)
+conn.commit()
+
+
+def load_users():
+    c.execute("SELECT username, password FROM users")
+    return dict(c.fetchall())
+
+
+class Handler(socketserver.BaseRequestHandler):
+    clients = {}  # 保存所有客户端的socket
+
+    def setup(self):
+        self.username = ""
+        self.file_peer = ""
+        self.authed = False  # authed表示是否已经登录
+        self.conn = sqlite3.connect("chat.db")
+        self.c = self.conn.cursor()
+
+    def handle(self):
+        while True:
+            data = utils.recv(self.request)
+            # 如果没有登录
+            if not self.authed:
+                self.user = data["username"]
+                if data["action"] == "login":
+                    if self.validate(data["username"], data["password"]):
+                        utils.send(self.request, {"response": "ok"})
+                        self.authed = True
+                        for user in Handler.clients.keys():
+                            # 发送给其他客户端，有新用户加入
+                            utils.send(
+                                Handler.clients[user].request,
+                                {"type": "person_join", "object": self.user},
+                            )
+                        Handler.clients[self.user] = self  # 将客户端加入clients字典
+                    elif self.isUserExist(data["username"]):
+                        utils.send(
+                            self.request,
+                            {"response": "fail", "reason": "用户不存在"},
+                        )
+                    else:
+                        utils.send(
+                            self.request,
+                            {"response": "fail", "reason": "密码错误"},
+                        )
+                elif data["action"] == "register":
+                    print(data)
+                    if self.register(data["username"], data["password"]):
+                        utils.send(self.request, {"response": "ok"})
+                    else:
+                        utils.send(
+                            self.request, {"response": "fail", "reason": "账号已存在！"}
+                        )
+            else:
+                if data["cmd"] == "get_users":
+                    users = []
+                    for user in Handler.clients.keys():
+                        if user != self.user:
+                            users.append(user)
+                    utils.send(self.request, {"type": "get_users", "data": users})
+                elif data["cmd"] == "get_history":
+                    utils.send(
+                        self.request,
+                        {
+                            "type": "get_history",
+                            "peer": data["peer"],
+                            "data": self.get_history(self.user, data["peer"]),
+                        },
+                    )
+                elif data["cmd"] == "chat" and data["peer"] != "":
+                    utils.send(
+                        Handler.clients[data["peer"]].request,
+                        {"type": "msg", "peer": self.user, "msg": data["msg"]},
+                    )
+                    self.append_history(self.user, data["peer"], data["msg"])
+                elif data["cmd"] == "chat" and data["peer"] == "":
+                    for user in Handler.clients.keys():
+                        if user != self.user:
+                            utils.send(
+                                Handler.clients[user].request,
+                                {
+                                    "type": "broadcast",
+                                    "peer": self.user,
+                                    "msg": data["msg"],
+                                },
+                            )
+                    self.append_history(self.user, "", data["msg"])
+                elif data["cmd"] == "close":
+                    self.finish()
+
+    def finish(self):
+        if self.authed:
+            self.authed = False
+            if self.user in Handler.clients.keys():
+                del Handler.clients[self.user]
+            for user in Handler.clients.keys():
+                utils.send(
+                    Handler.clients[user].request,
+                    {"type": "peer_left", "peer": self.user},
+                )
+        self.conn.close()
+
+    def register(self, usr, pwd):
+        if usr not in users.keys():
+            self.c.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)", (usr, pwd)
+            )
+            self.conn.commit()
+            return True
+        else:
+            return False
+
+    def validate(self, usr, pwd):
+        self.c.execute("SELECT password FROM users WHERE username=?", (usr,))
+        result = self.c.fetchone()
+        if result and result[0] == pwd:
+            return True
+        return False
+
+    def isUserExist(self, usr):
+        self.c.execute("SELECT username FROM users WHERE username=?", (usr,))
+        result = self.c.fetchone()
+        if result:
+            return True
+        return False
+
+    def append_history(self, sender, receiver, msg):
+        timestamp = time.strftime("%m月%d日%H:%M", time.localtime(time.time()))
+        self.c.execute(
+            "INSERT INTO history (sender, receiver, timestamp, message) VALUES (?, ?, ?, ?)",
+            (sender, receiver, timestamp, msg),
+        )
+        self.conn.commit()
+
+    def get_history(self, sender, receiver):
+        self.c.execute(
+            "SELECT sender, timestamp, message FROM history WHERE (sender=? AND receiver=?) OR (sender=? AND receiver=?)",
+            (sender, receiver, receiver, sender),
+        )
+        return self.c.fetchall()
+
+
+if __name__ == "__main__":
+    users = load_users()
+    app = socketserver.ThreadingTCPServer(("0.0.0.0", 12345), Handler)
+    app.serve_forever()
